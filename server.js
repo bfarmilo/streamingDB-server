3b0c9f44d5f1711ef975e8916749c428c6908469
Bill Farmilo <bill@farmilostrategy.com>
2018-05-11 18:22:51 -0400

    Basic server working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const config = require('./config/config.json');
 
 const app = express();
 
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
 const header = process.argv[2] === '-dev' ? config.test_url : config.app_url;
 console.info('using mode %s, adding CORS to origin %s', process.argv[2], header);
